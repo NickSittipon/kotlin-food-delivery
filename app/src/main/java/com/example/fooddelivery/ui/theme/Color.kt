f860b102e6982029fec1927be21354c5f53ad0bf
Sittipon Kimhouy <sittipon.k@rbsc.org>
2025-07-10 14:29:35 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/fooddelivery/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.example.fooddelivery.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+@Immutable
+data class AppColors(
+    val background: Color,
+    val onBackground: Color,
+    val onSurface: Color,
+    val secondarySurface: Color,
+    val regularSurface: Color,
+    val onRegularSurface: Color,
+    val actionSurface: Color,
+    val onActionSurface: Color,
+    val hightlightSurfaca: Color,
+    val onHightlightSurface:Color
+)
