58851118adfd4b53c4c26c03a8eb6d6f202f9f75
Sittipon Kimhouy <sittipon.k@rbsc.org>
2025-07-10 15:11:13 +0700

    feat(theme): add AppColors and color setup

--- app/src/main/java/com/example/fooddelivery/ui/theme/Color.kt
+++ app/src/main/java/com/example/fooddelivery/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.example.fooddelivery.ui.theme
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -15,12 +16,48 @@ val Pink40 = Color(0xFF7D5260)
 data class AppColors(
     val background: Color,
     val onBackground: Color,
+    val surface: Color,
     val onSurface: Color,
     val secondarySurface: Color,
+    val onSecondarySurface: Color,
     val regularSurface: Color,
     val onRegularSurface: Color,
     val actionSurface: Color,
     val onActionSurface: Color,
     val hightlightSurfaca: Color,
     val onHightlightSurface:Color
+)
+
+val LocalAppcolors = staticCompositionLocalOf {
+    AppColors(
+        background = Color.Unspecified,
+        onBackground = Color.Unspecified,
+        surface =  Color.Unspecified,
+        onSurface = Color.Unspecified,
+        secondarySurface = Color.Unspecified,
+        onSecondarySurface =  Color.Unspecified,
+        regularSurface = Color.Unspecified,
+        onRegularSurface = Color.Unspecified,
+        actionSurface = Color.Unspecified,
+        onActionSurface = Color.Unspecified,
+        hightlightSurfaca = Color.Unspecified,
+        onHightlightSurface = Color.Unspecified
+
+    )
+}
+
+val extendedColor = AppColors(
+    background = Color.White,
+    onBackground = Dark,
+    surface = Color.White,
+    onSurface = Dark,
+    secondarySurface = Pink,
+    onSecondarySurface = Color.White,
+    regularSurface = YellowLight,
+    onRegularSurface = Dark,
+    actionSurface = Yellow,
+    onActionSurface = Pink,
+    hightlightSurfaca = Green,
+    onHightlightSurface = Color.White,
+
 )
