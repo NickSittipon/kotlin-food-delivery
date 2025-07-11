06ed8de16843d7d3be225f8a104985b97a174f7d
NickSittipon <sittipon5725@gmail.com>
2025-07-11 11:59:44 +0700

    feat(ui): add ProductDescription

--- app/src/main/java/com/example/fooddelivery/ui/screen/ProductDetailsScreen.kt
+++ app/src/main/java/com/example/fooddelivery/ui/screen/ProductDetailsScreen.kt
@@ -2,19 +2,23 @@ package com.example.fooddelivery.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.data.ProductDescriptionData
 import com.example.fooddelivery.data.ProductFlavorState
 import com.example.fooddelivery.data.ProductFlavorsData
 import com.example.fooddelivery.data.ProductNutritionData
 import com.example.fooddelivery.data.ProductNutritionState
 import com.example.fooddelivery.data.ProductPreviewState
 import com.example.fooddelivery.ui.screen.components.FlavorSection
+import com.example.fooddelivery.ui.screen.components.ProductDescriptionSection
 import com.example.fooddelivery.ui.screen.components.ProductNutritionSection
 import com.example.fooddelivery.ui.screen.components.ProductPreviewSection
 
@@ -24,7 +28,8 @@ fun ProductDetailsScreen(
     modifier: Modifier = Modifier,
     productPreviewState: ProductPreviewState = ProductPreviewState(),
     productFlavors: List<ProductFlavorState> = ProductFlavorsData,
-    productNutritionState: ProductNutritionState = ProductNutritionData
+    productNutritionState: ProductNutritionState = ProductNutritionData,
+    productDescription: String = ProductDescriptionData
 ) {
     val scrollableState = rememberScrollState()
     
@@ -48,6 +53,16 @@ fun ProductDetailsScreen(
         ProductNutritionSection(
             modifier =Modifier.padding(horizontal = 18.dp),
             state = productNutritionState
+        )
+        Spacer(
+            modifier = Modifier.height(32.dp)
+        )
+        ProductDescriptionSection(
+            productDescription = productDescription,
+            modifier = Modifier
+                .navigationBarsPadding()
+                .padding(horizontal = 18.dp)
+                .padding(bottom = 24.dp)
         )
     }
 }
